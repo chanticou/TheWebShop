c9516a0b0aabe3adcace02f4b20f7cb5d43f875e
Chantal <chanticou@gmail.com>
2023-05-19 08:49:24 -0300

    responsive added

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,37 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "animate.css";
-import "aos/dist/aos.css";
-import { useRef } from "react";
 import styles from "@/styles/HomePage.module.css";
 
-export const HomePage = ({ navbar }) => {
+export const HomePage = ({ navbar, changeLanguaje }) => {
+  const [languajes, setLanguajes] = useState({});
   const [title, setTitle] = useState("");
-  const titleText = `PAGINAS WEB PERSONALIZADAS`;
+
   const sectionRef = useRef(null);
 
   useEffect(() => {
-    let i = 0;
-    const intervalId = setInterval(() => {
-      setTitle(titleText.substring(0, i));
-      i++;
-      if (i > titleText.length) {
-        clearInterval(intervalId);
-      }
-    }, 100);
-  }, []);
+    if (languajes) {
+      const titleText = `${changeLanguaje.customWebsites}`;
+      let i = 0;
+      const intervalId = setInterval(() => {
+        setTitle(titleText.substring(0, i));
+        i++;
+        if (i > titleText.length) {
+          clearInterval(intervalId);
+        }
+        setLanguajes(changeLanguaje);
+      }, 100);
+    }
+  }, [changeLanguaje]);
 
   const handleButtonClick = () => {
     sectionRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
   };
+
   return (
     <>
       <div className={styles.contentDivs}>
         <div className={styles.divOne}>
-          <h2 className={styles.subtitle}>Diseño</h2>
+          <h2 className={styles.subtitle}>{languajes.design}</h2>
           <h3 className={styles.subtitleDos}>
-            De calidad y al mejor precio del mercado
+            {languajes.quality}
             <br />
           </h3>
           <h3
@@ -45,14 +49,14 @@ export const HomePage = ({ navbar }) => {
                 margin: "auto",
               }}
             >
-              Desde $10.000
+              {languajes.from} $10.000
             </span>
           </h3>
           <button
             onClick={handleButtonClick}
             className={`${styles.buttonKnowMore} animate__animated animate__backInDown`}
           >
-            Conocé más
+            {languajes.learnMore}
           </button>
         </div>
 
@@ -66,16 +70,23 @@ export const HomePage = ({ navbar }) => {
               <img
                 className={styles.imageBackground}
                 src="https://res.cloudinary.com/dg05pzjsq/image/upload/v1683921343/pexels-lumn-399161_yjj5s4.jpg"
-                alt="backgorund image"
+                alt="background image"
               ></img>
             </div>
           }
         </div>
       </div>
 
-      <div ref={sectionRef} style={{ marginTop: "1000px" }}>
-        Sección hacia la que se desplazará
+      <div id="about" ref={sectionRef} style={{ marginTop: "600px" }}>
+        <h2>Nosotros</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+          rerum? Cumque aliquid perspiciatis minima vitae quasi sequi deserunt
+          tempore adipisci, libero fugiat, ea repellendus laborum. Vel mollitia
+          enim totam illo?
+        </p>
       </div>
+      <div style={{ marginTop: "600px" }}></div>
     </>
   );
 };
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,92 @@
-import styles from "@/styles/NavBar.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { HomePage } from "../HomePage";
+import styles from "@/styles/NavBar.module.css";
 import "tailwindcss/tailwind.css";
 
 export const NavBar = () => {
+  // Traducciones en diferentes idiomas
+  const translations = {
+    es: {
+      us: "Nosotros",
+      blogs: "Blogs",
+      contact: "Contacto",
+      proyect: "Proyectos",
+      design: "Diseño",
+      quality: "De calidad al mejor precio del mercado",
+      from: "Desde",
+      learnMore: "Conoce más",
+      customWebsites: "Páginas web personalizadas",
+    },
+    en: {
+      us: "Us",
+      blogs: "Blogs",
+      contact: "Contact",
+      proyect: "Projects",
+      design: "Design",
+      quality: "Quality at the best market price",
+
+      from: "From",
+      learnMore: "Learn more",
+      customWebsites: "Custom Websites",
+    },
+    fr: {
+      us: "Nous",
+      blogs: "Blogs",
+      contact: "Contact",
+      proyect: "Projets",
+      design: "Conception",
+      quality: "De qualité au meilleur prix du marché",
+
+      from: "À partir de",
+      learnMore: "En savoir plus",
+      customWebsites: "Sites web personnalisés",
+    },
+  };
+
   const [navbar, setNavbar] = useState(false);
+
+  const [changeLanguaje, setChangeLanguaje] = useState({
+    us: "Nosotros",
+    blogs: "Blogs",
+    contact: "Contacto",
+    proyect: "Proyectos",
+    design: "Diseño",
+    quality: "De calidad al mejor precio del mercado",
+    from: "Desde",
+    learnMore: "Conoce más",
+    customWebsites: "Páginas web personalizadas",
+  });
+
+  const [languaje, setLanguaje] = useState([
+    { code: "es", name: "Español" },
+    { code: "en", name: "English" },
+    { code: "fr", name: "French" },
+  ]);
+
+  const handleChangeLanguaje = (e) => {
+    if (languaje) {
+      if (e.target.value === "es") {
+        setChangeLanguaje(translations.es);
+      }
+      if (e.target.value === "en") {
+        setChangeLanguaje(translations.en);
+      }
+      if (e.target.value === "fr") {
+        setChangeLanguaje(translations.fr);
+      }
+    }
+  };
+  // console.log(changeLanguaje);
   return (
     <>
       <div>
         <nav className="w-full  sticky top-0 left-0 right-0 z-10">
           <div className="justify-center px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
             <div>
-              <div className="flex items-center flex-col justify-between py-3 md:py-5 md:block">
+              <div className="flex items-center justify-between py-3 md:py-5 md:block">
                 {/* LOGO */}
 
                 {/* HAMBURGER BUTTON FOR MOBILE */}
@@ -40,13 +112,13 @@ export const NavBar = () => {
               >
                 <ul className="p-0 md:h-auto items-center justify-justify-between md:flex ">
                   <li className="text-xl text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                    <Link href="#about" onClick={() => setNavbar(!navbar)}>
-                      Nosotros
-                    </Link>
+                    {/* <Link href="#about" onClick={() => setNavbar(!navbar)}> */}
+                    {changeLanguaje.us}
+                    {/* </Link> */}
                   </li>
                   <li className="text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                     <Link href="#blog" onClick={() => setNavbar(!navbar)}>
-                      Blogs
+                      {changeLanguaje.blogs}
                     </Link>
                   </li>
                   <Link href="/">
@@ -60,38 +132,41 @@ export const NavBar = () => {
                   </Link>
                   <li className="text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                     <Link href="#contact" onClick={() => setNavbar(!navbar)}>
-                      Contact
+                      {changeLanguaje.contact}
                     </Link>
                   </li>
                   <li className="text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                     <Link href="#projects" onClick={() => setNavbar(!navbar)}>
-                      Projects
+                      {changeLanguaje.proyect}
                     </Link>
                   </li>
                 </ul>
               </div>
             </div>
+            <div
+              style={{
+                position: "absolute",
+                right: "20px",
+              }}
+            >
+              <label style={{ marginRight: "10px" }}>Idioma</label>
+              <select
+                style={{ backgroundColor: "transparent", cursor: "pointer" }}
+                onChange={(e) => handleChangeLanguaje(e)}
+              >
+                {languaje.map((el) => {
+                  return (
+                    <option key={el.code} value={el.code}>
+                      {el.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
           </div>
         </nav>
-        <HomePage navbar={navbar} />
+        <HomePage navbar={navbar} changeLanguaje={changeLanguaje} />
       </div>
-      {/* <ul className={styles.contentNavBar}>
-        <li>
-          <button className={styles.burguerButton}>BURGUER BUTTON</button>
-        </li>
-
-        <li className={styles.navBarTitle}>
-          <p className={styles.navBarTitle}>Nosotros</p>
-        </li>
-        <li className={styles.navBarTitle}>
-          <div>
-            <img className={styles.image} src="/logo1png.png" alt=""></img>
-          </div>
-        </li>
-        <li className={styles.navBarTitle}>
-          <p>Contacto</p>
-        </li>
-      </ul> */}
     </>
   );
 };
